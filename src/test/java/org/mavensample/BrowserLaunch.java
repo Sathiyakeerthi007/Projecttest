d4333f4def56c7df333693b4458e4aa55c24527c
Sathiyakeerthi007 <sathiyakeerthi07@gmail.com>
2022-12-22 23:15:49 +0530

    project

---
+++ src/test/java/org/mavensample/BrowserLaunch.java
@@ -0,0 +1,19 @@
+package org.mavensample;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserLaunch {
+
+	public static void main(String[] args) {
+WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+		System.out.println("Take Completed");
+	}
+}
